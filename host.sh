3473f82a3808a47936958cff48078abb8e66690a
mitoop <me@mitoop.com>
2022-11-01 18:14:09 +0800

    init
    Signed-off-by: mitoop <me@mitoop.com>

---
+++ host.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Host
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🦋
+# @raycast.packageName Host
+
+# Documentation:
+# @raycast.author Mitoop
+# @raycast.authorURL https://github.com/mitoop
+
+/usr/local/bin/code /etc/hosts
---
+++ ocr.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Ocr
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🦋
+# @raycast.packageName Ocr
+# @raycast.needsConfirmation false
+
+# Documentation:
+# @raycast.description Ocr
+# @raycast.author Mitoop
+# @raycast.authorURL https://github.com/mitoop
+
+/usr/local/bin/ocr
+
