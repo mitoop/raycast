3e921745c291888f1c70b64f12173c4f15de6678
mitoop <me@mitoop.com>
2022-11-01 20:33:49 +0800

    ocr
    Signed-off-by: mitoop <me@mitoop.com>

--- ocr.sh
+++ ocr.sh
@@ -2,16 +2,16 @@
 
 # Required parameters:
 # @raycast.schemaVersion 1
-# @raycast.title Ocr
+# @raycast.title 文字识别
 # @raycast.mode compact
 
 # Optional parameters:
 # @raycast.icon 📸
-# @raycast.packageName Ocr
+# @raycast.packageName 文字识别
 # @raycast.needsConfirmation false
 
 # Documentation:
-# @raycast.description Ocr
+# @raycast.description Optical Character Recognition 文字识别
 # @raycast.author Mitoop
 # @raycast.authorURL https://github.com/mitoop
 
